bc5f1af2f8bca9239b1e7ced826b6df5d7f44558
serene <dmelloserene08@gmail.com>
2025-01-21 00:03:16 +0530

    second

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,7 @@
-import React from "react";
-import VoiceInput from "../components/VoiceToText";
-import HeatMap from "../components/Heatmap";
-import HeatMap2 from "../components/Heatmap2";
+
 import Sos from "../components/Sos";
 import OurFeatures from "../components/OurFeatures";
-import OnSeriesItemClick from "../components/Analytics";
+
 import Analytics from "../components/Analytics";
 
 
