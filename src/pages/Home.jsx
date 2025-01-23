fc2693510d56ad9dc4de75493175215a35a2652e
Shane-Dias <160272459+Shane-Dias@users.noreply.github.com>
2025-01-23 23:45:35 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,18 @@
+import React from "react";
 import Sos from "../components/Sos";
 import OurFeatures from "../components/OurFeatures";
 
 import Analytics from "../components/Analytics";
 import Footer from "../components/Footer";
 import FAQSection from "@/components/FAQ";
+// import EmergencyGuidelines from "@/components/EmergencyGuidelines";
 
 const Home = () => {
   return (
     <div>
       <Sos />
       <OurFeatures />
+      {/* <EmergencyGuidelines /> */}
       <Analytics />
       <FAQSection />
       <Footer />
