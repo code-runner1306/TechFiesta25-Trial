bfd80a664c9d11252f877ec5f32eb1164ba39911
Shane-Dias <160272459+Shane-Dias@users.noreply.github.com>
2025-01-24 11:18:15 +0530

    commit

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -14,6 +14,7 @@ import Footer from "@/components/Footer";
 import { useAuth } from "@/context/AuthContext";
 
 const UserDashboard = () => {
+  const navigate = useNavigate();
   const { logout } = useAuth();
 
   const dashboardStats = {
