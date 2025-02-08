68ae0c4359e160e0a6cf12f5ce86618054ca7715
serene <dmelloserene08@gmail.com>
2025-02-08 18:37:16 +0530

    report false added

--- backend/incidents/views.py
+++ backend/incidents/views.py
@@ -494,11 +494,7 @@ def update_incident(request, id):
         user = incident.reported_by
         print("information gotten")
         # Send Email Notification
-<<<<<<< HEAD
         # subject = f"Incident Status Updated: {incident.id}"
-=======
-        #subject = f"Incident Status Updated: {incident.id}"
->>>>>>> 21e21ebe2b7d0eb3f97d287d70c3757520a3e2a4
         # message = f"Dear {user.first_name},\n\nThe status of your reported incident (ID: {incident.id}) has been updated to: {status}.\n\nThank you,\nIncident Management Team"
         # recipient_email = user.email  # Get user email
         # print("Information recieved")
