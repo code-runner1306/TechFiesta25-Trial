4c20cf78c96090017d9821f46d88d1d68c92f05f
Shane-Dias <160272459+Shane-Dias@users.noreply.github.com>
2025-02-09 02:57:11 +0530

    filter based on searched location working letss goo

--- src/pages/RecentIncidents1.jsx
+++ src/pages/RecentIncidents1.jsx
@@ -47,7 +47,7 @@ const RecentIncidents = () => {
   useEffect(() => {
     if (!isMapModalOpen) return;
 
-    const map = L.map("map").setView([51.505, -0.09], 13);
+    const map = L.map("map").setView([20.5937, 78.9629], 5);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
